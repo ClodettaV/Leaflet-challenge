d335c5e824fbc264707866a078c380ca43c86995
Claudia <valenticlaudia85@gmail.com>
2022-01-22 17:07:00 +0800

    first commit

---
+++ Leaflet-Step-1/static/js/config.js
@@ -0,0 +1,2 @@
+//API key
+API_KEY = "pk.eyJ1IjoiY3ZhbCIsImEiOiJja3kydTQ2czkwb2lkMndwZDFhb3lvaHY2In0.uB8gBm60AQJk0DUD2LBeng";
---
+++ Leaflet-Step-1/static/js/logic.js
@@ -0,0 +1,46 @@
+// Store our API endpoint inside queryUrl
+var queryUrl = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson';
+
+//Perform a GET request to the query URL
+d3.json(queryUrl).then(function(data) {
+  // Once we get a response, send the data.features object to the createFeatures function
+  createFeatures(data.features);
+});
+
+
+function createFeatures(earthquakeData) {
+
+  // Define a function we want to run once for each feature in the features array
+  // Give each feature a popup describing the place and time of the earthquake
+  function onEachFeature(feature, layer) {
+    layer.bindPopup("<h3>" + feature.properties.place +
+      "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+  }
+
+  // Create a GeoJSON layer containing the features array on the earthquakeData object
+  // Run the onEachFeature function once for each piece of data in the array
+  var earthquakes = L.geoJSON(earthquakeData, {
+    onEachFeature: onEachFeature
+  });
+
+  // Sending our earthquakes layer to the createMap function
+  createMap(earthquakes);
+}
+
+
+// Create a map object
+var myMap = L.map("map", {
+  center: [15.5994, -28.6731],
+  zoom: 3
+});
+
+
+L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/*satellite-v9*/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='https://docs.mapbox.com/mapbox-gl-js/example/setstyle/'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/satellite-v9",
+  accessToken: API_KEY
+}).addTo(myMap);
+
