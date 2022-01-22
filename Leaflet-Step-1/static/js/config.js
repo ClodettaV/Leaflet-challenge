3dfc2c75b0c9a121bdd010a67a05b2e7f7a74df5
Claudia <valenticlaudia85@gmail.com>
2022-01-22 20:55:34 +0800

    first update

--- Leaflet-Step-1/static/js/config.js
+++ Leaflet-Step-1/static/js/config.js
@@ -1,2 +1,2 @@
 //API key
-API_KEY = "pk.eyJ1IjoiY3ZhbCIsImEiOiJja3kydTQ2czkwb2lkMndwZDFhb3lvaHY2In0.uB8gBm60AQJk0DUD2LBeng";
+API_KEY = "pk.eyJ1IjoiY3ZhbCIsImEiOiJja3lwdHlibXcwZTEwMm9tdjh1Ynk4bGFlIn0.rylNlKly3ClY5j2P6b-i3Q";
--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,12 +1,12 @@
 // Store our API endpoint inside queryUrl
 var queryUrl = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson';
 
+
 //Perform a GET request to the query URL
 d3.json(queryUrl).then(function(data) {
   // Once we get a response, send the data.features object to the createFeatures function
   createFeatures(data.features);
 });
-
 
 function createFeatures(earthquakeData) {
 
@@ -25,22 +25,33 @@ function createFeatures(earthquakeData) {
 
   // Sending our earthquakes layer to the createMap function
   createMap(earthquakes);
-}
+   }
 
+   function createMap(earthquakes) {
 
-// Create a map object
-var myMap = L.map("map", {
-  center: [15.5994, -28.6731],
-  zoom: 3
-});
-
-
-L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/*satellite-v9*/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+var satellite_map = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/*satellite-v9*/tiles/{z}/{x}/{y}?access_token={accessToken}', {
   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='https://docs.mapbox.com/mapbox-gl-js/example/setstyle/'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
   tileSize: 512,
   maxZoom: 18,
   zoomOffset: -1,
   id: "mapbox/satellite-v9",
   accessToken: API_KEY
-}).addTo(myMap);
+// }).addTo(myMap);
+});
 
+// Define a baseMaps object to hold our base layers
+var baseMaps = {
+  "Satellite map": satellite_map
+  };
+
+// Create a map object
+var myMap = L.map("map", {
+  center: [15.5994, -28.6731],
+  zoom: 3,
+  layers: [satellite_map]
+});
+
+L.control.layers(baseMaps,{
+  // collapsed: false
+}).addTo(myMap);
+}
