ccb0f4ce017f77e97d539794b9294c2638327368
Claudia <valenticlaudia85@gmail.com>
2022-01-23 17:28:46 +0800

    updated earthquakes

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -2,56 +2,164 @@
 var queryUrl = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson';
 
 
-//Perform a GET request to the query URL
-d3.json(queryUrl).then(function(data) {
-  // Once we get a response, send the data.features object to the createFeatures function
-  createFeatures(data.features);
-});
+// //Perform a GET request to the query URL
+// d3.json(queryUrl).then(function(data) {
+//   // Once we get a response, send the data.features object to the createFeatures function
+//   createFeatures(data.features);
+// });
 
-function createFeatures(earthquakeData) {
+// function createFeatures(earthquakeData) {
 
-  // Define a function we want to run once for each feature in the features array
-  // Give each feature a popup describing the place and time of the earthquake
-  function onEachFeature(feature, layer) {
-    layer.bindPopup("<h3>" + feature.properties.place +
-      "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
-  }
+//   // Define a function we want to run once for each feature in the features array
+//   // Give each feature a popup describing the place and time of the earthquake
+//   function onEachFeature(feature, layer) {
+//     layer.bindPopup("<h3>" + feature.properties.place +
+//       "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+//   }
 
-  // Create a GeoJSON layer containing the features array on the earthquakeData object
-  // Run the onEachFeature function once for each piece of data in the array
-  var earthquakes = L.geoJSON(earthquakeData, {
-    onEachFeature: onEachFeature
-  });
+//   // Create a GeoJSON layer containing the features array on the earthquakeData object
+//   // Run the onEachFeature function once for each piece of data in the array
+//   var earthquakes = L.geoJSON(earthquakeData, {
+//     onEachFeature: onEachFeature
+//   });
 
-  // Sending our earthquakes layer to the createMap function
-  createMap(earthquakes);
-   }
+//   // Sending our earthquakes layer to the createMap function
+//   createMap(earthquakes);
+//    }
 
    function createMap(earthquakes) {
 
-var satellite_map = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/*satellite-v9*/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+var graymap = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='https://docs.mapbox.com/mapbox-gl-js/example/setstyle/'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/light-v10",
+  accessToken: API_KEY
+    
+});
+var satellite_map = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/{z}/{x}/{y}?access_token={accessToken}', {
   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='https://docs.mapbox.com/mapbox-gl-js/example/setstyle/'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
   tileSize: 512,
   maxZoom: 18,
   zoomOffset: -1,
   id: "mapbox/satellite-v9",
   accessToken: API_KEY
-// }).addTo(myMap);
+
 });
+var satellite_map = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='https://docs.mapbox.com/mapbox-gl-js/example/setstyle/'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/satellite-v9",
+  accessToken: API_KEY
+
+});
+
 
 // Define a baseMaps object to hold our base layers
 var baseMaps = {
+  "Graymap": graymap,
   "Satellite map": satellite_map
   };
+
+// Create overlay object to hold our overlay layer
+var overlayMaps = {
+  Earthquakes: earthquakes
+};
 
 // Create a map object
 var myMap = L.map("map", {
   center: [15.5994, -28.6731],
   zoom: 3,
-  layers: [satellite_map]
+  layers: [satellite_map, graymap]
 });
 
-L.control.layers(baseMaps,{
+L.control.layers(baseMaps, overlayMaps, {
   // collapsed: false
 }).addTo(myMap);
+}
+
+/////////////////////////////////////////////////////////////////////////////
+
+//Perform a GET request to the query URL
+d3.json(queryUrl).then(function(data) {
+  // Once we get a response, send the data.features object to the createFeatures function
+  createFeatures(data.features);
+});
+
+
+// Function to Determine Size of Marker Based on the Magnitude of the Earthquake
+    function markerSize(magnitude) {
+        if (magnitude === 0) {
+          return 1;
+        }
+        return magnitude * 3;
+    }
+    // Function to Determine Style of Marker Based on the Magnitude of the Earthquake
+    function styleInfo(feature) {
+        return {
+          opacity: 1,
+          fillOpacity: 1,
+          fillColor: chooseColor(feature.properties.mag),
+          color: "#000000",
+          radius: markerSize(feature.properties.mag),
+          stroke: true,
+          weight: 0.5
+        };
+    }
+    // Function to Determine Color of Marker Based on the Magnitude of the Earthquake
+    function chooseColor(magnitude) {
+        switch (true) {
+        case magnitude > 5:
+            return "#581845";
+        case magnitude > 4:
+            return "#900C3F";
+        case magnitude > 3:
+            return "#C70039";
+        case magnitude > 2:
+            return "#FF5733";
+        case magnitude > 1:
+            return "#FFC300";
+        default:
+            return "#DAF7A6";
+        }
+    }
+
+
+
+
+
+
+function createFeatures(earthquakeData) {
+
+  // Define a function we want to run once for each feature in the features array
+  // Give each feature a popup describing the place and time of the earthquake
+  function onEachFeature(feature, layer) {
+    
+    layer.bindPopup("<h3>" + feature.properties.place +
+      "</h3><hr><p>" + new Date(feature.properties.time) +
+      "</p><hr><p>Magnitude: " + feature.properties.mag +  "</p>");
+  }
+
+
+
+
+  // Create a GeoJSON layer containing the features array on the earthquakeData object
+  // Run the onEachFeature function once for each piece of data in the array
+  var earthquakes = L.geoJSON(earthquakeData, {
+    onEachFeature: onEachFeature,
+    pointToLayer: function(feature, latlng) {
+      return L.circleMarker(latlng);
+  },
+  style: styleInfo,
+
+  // Add earthquakeData to earthquakes LayerGroups 
+}).addTo(earthquakes);
+// Add earthquakes Layer to the Map
+earthquakes.addTo(myMap);
+  // // Sending our earthquakes layer to the createMap function
+  // createMap(earthquakes);
+  //  }
 }
